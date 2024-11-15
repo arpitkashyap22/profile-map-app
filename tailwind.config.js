72031e707a7c18879a3c16810e046784250c19ec
Arpit Kashyap <arpitkashyap2000@gmail.com>
2024-11-15 10:45:15 +0530

    config change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#FF0000', // Red
-        secondary: '#000000', // Black
-      },
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {
+    colors: {
+      primary: '#FF0000', // Red
+      secondary: '#000000', // Black
     },
   },
-  plugins: [],
-}
+};
+export const plugins = [];
